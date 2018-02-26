f87dd17ec22bfe892c3b54c6f1c0fb4e96497ebc
faltosu <32680991+faltosu@users.noreply.github.com>
2018-02-26 18:46:31 +0100

    Proyecto inicial

---
+++ entrega1/src/test/java/sdi/entrega1/Entrega1ApplicationTests.java
@@ -0,0 +1,16 @@
+package sdi.entrega1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Entrega1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
