025538c9f03c1970a4062784e85863122959ba13
SergioFaya <estisergio@gmail.com>
2018-03-11 02:27:20 +0100

    template internacionalizada y hecho el service

---
+++ entrega1/src/main/java/sdi/entrega1/services/friends/request/FriendshipRequestService.java
@@ -0,0 +1,18 @@
+package sdi.entrega1.services.friends.request;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import sdi.entrega1.repositories.FriendShipRequestRepo;
+
+@Service
+public class FriendshipRequestService {
+
+	@Autowired
+	private FriendShipRequestRepo friendshipRepo;
+	
+	
+	
+	
+	
+}
